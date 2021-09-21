cc31a31669b315b65145d13b4fafc3b61ca3ca86
Your Name <you@example.com>
2021-09-21 11:42:36 +0800

    chore(): don't show progress

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var glob = require('glob');
 var fs = require('fs-extra');
 var chalk = require('chalk');
 const options = {}
+console.log(`开始执行删除任务`);
 glob("**/node_modules/", options, function (err, dirs) {
-  Promise.all(dirs.map(dir => fs.rm(dir, { recursive: true }).then(() => console.log(`\n${chalk.red('删除')} ${dir}`)))).then(() => {
+  Promise.all(dirs.map(dir => fs.rm(dir, { recursive: true }))).then(() => {
     console.log(`删除任务${chalk.green('成功')}`);
   }).catch(err => {
     console.log(`删除任务${chalk.red('失败')}`, err);
